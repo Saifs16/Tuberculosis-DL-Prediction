167ad40774d4dc935147c2390743d4e5076ddcd1
Saifs16 <saifsultan2013@gmail.com>
2023-07-11 12:24:22 +0400

    fix: Preditction works(#1)

--- server.py
+++ server.py
@@ -4,13 +4,11 @@ from flask import render_template
 import numpy as np
 import io
 from PIL import Image
+import cv2
 
 from keras.preprocessing.image import img_to_array
-from keras.applications import imagenet_utils
-from keras import backend as K
-
+from keras.models import load_model
 import tensorflow as tf
-from tensorflow.keras.models import load_model
 
 # Initialize the Flask app
 app = flask.Flask(__name__, template_folder='static')
@@ -19,75 +17,71 @@ response = None
 
 def load_our_model():
     global model
-    model = tf.keras.models.load_model('./tb-model.h5')
+    model = tf.keras.models.load_model('tb-model.h5')
 
 # Preprocessing the image
-def prepare_image(image, target):
-    # If the image is not of the RGB format, convert it
+def prepare_image(image, target_size):
+    # If the image mode is not RGB, convert it
     if image.mode != "RGB":
         image = image.convert("RGB")
-
-    # Resize the image and preprocess it
-    image = image.resize(target)
-    image = img_to_array(image)
-    image = np.expand_dims(image, axis=0)
-    image = imagenet_utils.preprocess_input(image)
-
+    
+    # Resize the image to the target size
+    image = image.resize(target_size)
+    image = np.array(image)
+    
+    # Ensure all images have 3 channels
+    if len(image.shape) == 2:
+        image = np.stack([image]*3, axis=-1)
+        
+    # Convert BGR to RGB
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = image/255.
+    
+    # Return the processed image
     return image
 
-# Home Page
 @app.route("/")
 def index():
     return flask.render_template('index.html')
 
-# Building the predict API endpoint
 @app.route("/predict", methods=["POST", "GET"])
 def predict():
-    # Ensure that the image was loaded properly on our endpoint
-    if flask.request.method == "POST" and flask.request.files.get("image"):
-        # Read image in PIL format
-        image = flask.request.files["image"].read()
-        image = Image.open(io.BytesIO(image))
-        # Preprocess the image for classification
-        image = prepare_image(image, target=(28, 28))
-        if image.shape[2] == 1:
-            image = np.dstack([image, image, image])
-        # Make prediction on the preprocessed image
-        image = np.array(image)
-        image = image / 255.0
+    data = {}  # dictionary to store results 
+    if flask.request.method == "POST":
+        if flask.request.files.get("image"):
+            image = flask.request.files["image"].read()
+            image = Image.open(io.BytesIO(image))
 
-        # Classify the input image and then initialize the list of predictions to return to the client
-        prediction = model.predict(np.array(image))
+            image = prepare_image(image, target_size=(28, 28))
 
-        print("PRED", prediction)
-        res = np.argmax(prediction)
-        print("RES", res)
+            # Predict the disease using the preprocessed image
+            predictions = model.predict(np.array([image]))
 
-        # If res is 0, then the image predicted is Normal else it is Tuberculosis
-        # Build the response and return it to the client
-        global response
-        if res == 0:
-            response = {
-                'prediction' : {
-                    'Normal': 1,
-                    'Tuberculosis': 0 
+            results = np.argmax(predictions, axis=1)
+
+            global response
+            
+            if results[0] == 0:
+                print('Predicted to be Normal')
+                response = {
+                    'prediction' : {
+                        'Normal': 1,
+                        'Tuberculosis': 0 
+                    }
                 }
-            }
-        else:
-            response = {
-                'prediction' : {
-                    'Normal': 0,
-                    'Tuberculosis': 1 
+            elif results[0] == 1:
+                print('Predicted to be Tuberculosis')
+                response = {
+                    'prediction' : {
+                        'Normal': 0,
+                        'Tuberculosis': 1 
+                    }
                 }
-            }
 
-        print("response", response)
         return render_template('results.html', response=response)
 
-# If this is the main thread of execution first load the model and then start the server
 if __name__ == "__main__":
-    print(("* Loading model and starting Flask server..."
-        "please wait until server has fully started"))
+    print(("* Loading Keras model and Flask starting server..."
+           "please wait until server has fully started"))
     load_our_model()
-    #app.debug = True
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
